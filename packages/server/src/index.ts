62b247223b9f34a3d512a133f36dfa6e9926306c
Guilherme <gui.mochila@gmail.com>
2021-11-17 11:59:17 +0100

    Small change to trigger actions

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'ok' });
+  res.json({ message: 'okay' });
 });
 
 app.listen(port, () => {
